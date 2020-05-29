8b2ada95ccd6d1ed30b346c0781582671e99e0eb
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-05-29 15:04:51 +0900

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -70,13 +70,14 @@ class Music(commands.Cog):
             track = results['tracks'][0]
             embed.title = ':arrow_forward: | 음악 재생!'
             embed.description = f'[{track["info"]["title"]}]({track["info"]["uri"]})'
-            player.add(requester=ctx.author.id, track=track)
             info = track['info']
+            track = lavalink.models.AudioTrack(track, ctx.author.id, recommended=True)
+            player.add(requester=ctx.author.id, track=track)
         embed.set_image(url="http://img.youtube.com/vi/%s/0.jpg" %(info['identifier']))
         embed.set_footer(text=BOT_NAME_TAG_VER)
         await ctx.send(embed=embed)
-        #if not player.is_playing:
-        await player.play()
+        if not player.is_playing:
+            await player.play()
 
     @commands.command(aliases=['탐색'])
     async def seek(self, ctx, *, seconds: int):
