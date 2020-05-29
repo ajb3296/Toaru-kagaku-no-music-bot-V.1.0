a4cf476667619633da96aca5beda5093a2ae0281
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-05-29 23:40:54 +0900

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,5 @@ BOT_VER = "V.1.0"
 BOT_ID = 714140461840728144
 BOT_NAME_TAG_VER = "%s%s | %s" %(BOT_NAME, BOT_TAG, BOT_VER)
 color_code = 0xc68e6e
+
+lavalinklink = "https://github.com/Cog-Creators/Lavalink-Jars/releases/download/3.3.1_1050/Lavalink.jar"
