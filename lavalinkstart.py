28cddb79b2ffb229c65873b39b2eccb4cf4bebfe
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-05-30 16:10:15 +0900

    Update lavalinkstart.py

--- lavalinkstart.py
+++ lavalinkstart.py
@@ -5,4 +5,4 @@ from urllib import request
 def child_process(link):
     print(f"Child process PID : {multiprocessing.current_process().pid}")
     request.urlretrieve(link, "Lavalink.jar")
-    os.system("java -jar Lavalink.jar -Xmx350m -Xss512k -XX:CICompilerCount=2")
+    os.system("java -jar Lavalink.jar -XX:+UseContainerSupport -Xmx350m -Xss512k -XX:CICompilerCount=2 -Dfile.encoding=UTF-8")
