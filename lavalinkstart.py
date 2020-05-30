889cada1814b824ac140db85eb8dd2656bc932b4
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-05-30 12:32:41 +0900

    Update lavalinkstart.py

--- lavalinkstart.py
+++ lavalinkstart.py
@@ -6,6 +6,6 @@ def child_process(link):
     print(f"Child process PID : {multiprocessing.current_process().pid}")
     request.urlretrieve(link, "Lavalink.jar")
     additional_options = os.environ.get(
-            "-Xmx300m"
+            "-Xmx350m"
     )
     os.system("java -jar Lavalink.jar %s" %additional_options)
